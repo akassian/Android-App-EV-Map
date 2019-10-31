8a9db49df12ccb3728c441c7352d5beb21e1524a
Andrei Kassiantchouk <akass1122@gmail.com>
2019-10-31 14:59:12 -0700

    Added StationActivity and xml

---
+++ app/src/main/java/com/example/android/evmap/StationActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.evmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_station);
+    }
+}
